0954a69429c4fff6064787d2b1c20021305dea58
darwinum <dun34@hotmail.com>
2019-07-15 14:38:46 -0500

    resetear y agregar datos al formulario

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -11,7 +11,7 @@ export class DataComponent {
   forma: FormGroup;
 
   /* esta seria la representación para un objeto complejo */
-  usuario: Object = {
+  usuario: any = {
     nombrecompleto: {
       nombre: 'nombre dum',
       apellido: 'apellido'
@@ -20,7 +20,12 @@ export class DataComponent {
   };
 
   constructor() {
-    /* manejo de objetos complejos desde el formulario */
+    /* manejo de objetos complejos desde el formulario 
+    -en el formcontrol se agrega la propiedad con el fin que el sistema, cargue los datos por defecto.
+    -con esta linea se puede mostrar el valor de la propiedad por defecto en el formulario
+     nombre: new FormControl(this.usuario.nombrecompleto.nombre, [
+     lo anterior se quita para rellenar el formulario con una linea.
+    */
     this.forma = new FormGroup({
       nombrecompleto: new FormGroup(
         {
@@ -38,7 +43,14 @@ export class DataComponent {
         Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')
       ])
     });
+    /* linea para mostrar todos los datos de una vez en el formulario por defecto
+       esto es valido siempre y cuando el objeto tenga la misma estructura de FormGroup, en 
+       este caso el objeto forma.
+    */
+    this.forma.setValue(this.usuario);
 
+
+    /* Manejo del objeto simple sin valores anidados */
     /*  this.forma = new FormGroup({
        nombre: new FormControl('', [
          Validators.required,
@@ -56,6 +68,18 @@ export class DataComponent {
   }
   guardarCambios() {
     console.log(this.forma.value);
+    console.log(this.forma);
+    /* Resetear o borrar los datos del formulario despues de guardar */
+    this.borrarCampos();
   }
 
+  borrarCampos() {
+    this.forma.reset({
+      nombrecompleto: {
+        nombre: '',
+        apellidos: ''
+      },
+      correo: ''
+    });
+  }
 }
